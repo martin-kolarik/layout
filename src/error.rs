5e3e9a140889169ac321809fcdefb1f919ce4158
Martin Kolařík <martin.kolarik@smartcontrol.cz>
2023-03-07 12:27:36 +0100

    Bump up versions

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use allsorts::error::{ParseError, ReadWriteError, ShapingError};
+use allsorts::subset::SubsetError;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -9,7 +10,9 @@ pub enum Error {
     FontReadWrite(#[from] ReadWriteError),
     #[error("Font typeset error: {0}")]
     FontShaping(#[from] ShapingError),
-    #[error("Error in program logic: {0}")]
+    #[error("Font parse error: {0}")]
+    FontSubset(#[from] SubsetError),
+    #[error("Font subsetting error: {0}")]
     LockError(String),
     #[error("Font '{0}' cannot be read, it seems to be empty")]
     MalformedFont(String),
